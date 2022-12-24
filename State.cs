c0286e5dee3bb65d3a306709b06ed61241f3d2d8
Yaman Qassas <yaman102011@gmail.com>
2022-12-24 18:33:40 +0300

    Implement State class

---
+++ State.cs
@@ -0,0 +1,56 @@
+class State {
+	public Station station;
+	public int timeSpent, hp, money;
+
+	public State(Station station, int timeSpent, int hp, int money) {
+		this.station = station;
+		this.timeSpent = timeSpent;
+		this.hp = hp;
+		this.money = money;
+	}
+
+	public void choosePath(Connection path) {
+		this.station = path.targetStation;
+
+		// Wait for bus/taxi
+		if (path is BusConnection) {
+			this.timeSpent += this.station.busWaitTime;
+		} else if (path is TaxiConnection) {
+			this.timeSpent += this.station.taxiWaitTime;
+		}
+
+		// Ride the bus/taxi
+		this.timeSpent += path.getTimeChange();
+		this.hp += path.getHpChange();
+		this.money += path.getMoneyChange();
+	}
+
+	List<State> getNextStates() {
+		List<State> nextStates = new List<State>();
+
+		foreach (Connection connection in this.station.connections) {
+			if (this.canTakeConnection(connection)) {
+				State stateClone = this.clone();
+				stateClone.choosePath(connection);
+				nextStates.Add(stateClone);
+			}
+		}
+
+		return nextStates;
+	}
+
+	bool canTakeConnection(Connection path) {
+		int futureMoney = this.money + path.getMoneyChange();
+		int futureHp = this.hp + path.getHpChange();
+
+		return futureMoney <= Program.INITIAL_MONEY && futureHp > 0;
+	}
+	
+	public bool isFinal() {
+		return this.station == Program.HOME_STATION;
+	}
+
+	State clone() {
+		return new State(this.station, this.timeSpent, this.hp, this.money);
+	}
+}
