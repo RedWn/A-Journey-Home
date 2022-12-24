fefdfaa18c9722c87c6a11b4f7dee41a89bbd37b
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-24 22:44:21 +0300

    Print solution details after reaching home station

--- Solver.cs
+++ Solver.cs
@@ -1,28 +1,65 @@
-class Solver {
-	public void solve(State initialState) {
-		List<State> visited = new List<State>();
-		PriorityQueue<State, int> queue = new PriorityQueue<State, int>();
+static class Solver
+{
+    private static Dictionary<State, State?> _parents = new();
+    private static PriorityQueue<State, int> _queue = new();
+    private static HashSet<State> _visited = new();
 
-		bool shouldBreakLoop = false;
-		while (queue.Count > 0) {
-			if (shouldBreakLoop) break;
-			
-			State state = queue.Dequeue();
+    public static void Solve(State initialState)
+    {
 
-			if (visited.Contains(state)) continue;
-			visited.Add(state);
+        State? finalState = null;
+        _parents[initialState] = null;
+        _queue.Enqueue(initialState, initialState.TimeSpent);
 
-			List<State> nextStates = state.getNextStates();
+        bool shouldBreakLoop = false;
 
-			foreach (State nextState in nextStates) {
-				// Here, priority is timespent 
-				queue.Enqueue(nextState, nextState.timeSpent);
+        while (_queue.Count > 0)
+        {
+            if (shouldBreakLoop) break;
 
-				if (nextState.isFinal()) {
-					shouldBreakLoop = true;
-					break;
-				}
-			}
-		}
-	}
+            State state = _queue.Dequeue();
+            if (_visited.Contains(state)) continue;
+            _visited.Add(state);
+
+            List<State> nextStates = state.GetNextStates();
+            foreach (State nextState in nextStates)
+            {
+                int priority = nextState.TimeSpent;
+                _queue.Enqueue(nextState, priority);
+                _parents[nextState] = state;
+
+                if (nextState.IsFinal())
+                {
+                    shouldBreakLoop = true;
+                    finalState = nextState;
+                    break;
+                }
+            }
+        }
+
+        if (finalState is not null) printSolutionDetails(finalState);
+    }
+
+    private static void printSolutionDetails(State finalState)
+    {
+        // Generate student path
+        Stack<State> statesPath = new();
+
+        State s = _parents[finalState];
+        while (s is not null)
+        {
+            statesPath.Push(s);
+            s = _parents[s];
+        }
+
+        while (statesPath.Count > 0)
+        {
+            var state = statesPath.Pop();
+            Console.WriteLine($"Station: {state.Station.Name}");
+            Console.WriteLine($"HP: {state.AvailableHP}, Time Spent: {state.TimeSpent}, Money: {state.AvailableMoney}");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Home... after 5 years on the east cost, it was time to go home.");
+    }
 }
