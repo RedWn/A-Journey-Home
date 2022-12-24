0799a53b04355e5f80e032f48b62c37460e418ee
Yaman Qassas <yaman102011@gmail.com>
2022-12-24 17:55:13 +0300

    Add example stations and connections

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Station university = new Station("university", 10, 10);
+Station jaramana = new Station("jaramana", 20, 30);
+Station karajElSet = new Station("karaj el set", 10, 5);
+Station home = new Station("rukn al dein", 5, 30);
+
+university.addConnection(ConnectionType.BUS, jaramana, 200, 20);
+jaramana.addConnection(ConnectionType.BUS, karajElSet, 100, 70);
+karajElSet.addConnection(ConnectionType.BUS, home, 30, 3000);
