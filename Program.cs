44d8e4b75b1e9cf00e6743cda3e82e331c389e03
Yaman Qassas <yaman102011@gmail.com>
2022-12-24 18:13:19 +0300

    Create Program class and its static members

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿Station university = new Station("university", 10, 10);
-Station jaramana = new Station("jaramana", 20, 30);
-Station karajElSet = new Station("karaj el set", 10, 5);
-Station home = new Station("rukn al dein", 5, 30);
+﻿class Program {
+	public static Station HOME_STATION;
+	public static int INITIAL_MONEY = 100;
 
-university.addConnection(ConnectionType.BUS, jaramana, 200, 20);
-jaramana.addConnection(ConnectionType.BUS, karajElSet, 100, 70);
-karajElSet.addConnection(ConnectionType.BUS, home, 30, 3000);
+	public void Main() {
+		Station university = new Station("university", 10, 10);
+		Station jaramana = new Station("jaramana", 20, 30);
+		Station karajElSet = new Station("karaj el set", 10, 5);
+		HOME_STATION = new Station("rukn al dein", 5, 30);
+
+		university.addConnection(ConnectionType.BUS, jaramana, 200, 20);
+		jaramana.addConnection(ConnectionType.BUS, karajElSet, 100, 70);
+		karajElSet.addConnection(ConnectionType.BUS, HOME_STATION, 30, 3000);
+	}
+}
