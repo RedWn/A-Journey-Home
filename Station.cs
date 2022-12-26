a5e13bcb4ce49bbd9f3276a8d1ec093f1c7570b5
RedWn <redwaniscool@gmail.com>
2022-12-27 00:45:16 +0200

    Add location to Station

--- Station.cs
+++ Station.cs
@@ -1,36 +1,41 @@
-class Station {
-	public readonly string Name;
+class Station
+{
+    public readonly string Name;
 
-	public int TaxiWaitTime { get; private set; }
-	public int BusWaitTime { get; private set; }
+    public int TaxiWaitTime { get; private set; }
+    public int BusWaitTime { get; private set; }
+    public Location location = new Location();
 
-	public List<Connection> Connections;
+    public List<Connection> Connections;
 
-	public Station(string name, int taxiWaitTime, int busWaitTime) {
-		Name = name;
-		TaxiWaitTime = taxiWaitTime;
-		BusWaitTime = busWaitTime;
+    public Station(string name, int taxiWaitTime, int busWaitTime, Location l)
+    {
+        Name = name;
+        TaxiWaitTime = taxiWaitTime;
+        BusWaitTime = busWaitTime;
+        location = l;
 
-		Connections = new List<Connection>();
-	}
+        Connections = new List<Connection>();
+    }
 
-	public void AddConnection(ConnectionType type, Station targetStation, int distanceInKm, float speedInKph, string routeName = "") {
-		Connection connection = ConnectionFactory.GetConnection(type, targetStation, distanceInKm, speedInKph, routeName);
-		Connections.Add(connection);
-	}
+    public void AddConnection(ConnectionType type, Station targetStation, int distanceInKm, float speedInKph, string routeName = "")
+    {
+        Connection connection = ConnectionFactory.GetConnection(type, targetStation, distanceInKm, speedInKph, routeName);
+        Connections.Add(connection);
+    }
 
-	public int GetWaitingTime(Connection connection)
-	{
+    public int GetWaitingTime(Connection connection)
+    {
         if (connection.Type == ConnectionType.BUS)
         {
             return BusWaitTime;
-		}
+        }
 
         else if (connection.Type == ConnectionType.TAXI)
         {
             return TaxiWaitTime;
         }
 
-		return 0;
+        return 0;
     }
 }
