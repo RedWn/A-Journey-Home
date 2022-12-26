bc44ef21e36faf299cb0a579caa81ba28b633dad
RedWn <redwaniscool@gmail.com>
2022-12-27 00:46:33 +0200

    shut up errors

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@
 
     public static void Main()
     {
-        Station university = new Station("university", 1, 1);
-        Station jaramana = new Station("jaramana", 1, 1);
-        Station karajElSet = new Station("karaj el set", 1, 1);
+        // will need a rewrite to accomodate the new location requirment
+        // Station university = new Station("university", 1, 1);
+        // Station jaramana = new Station("jaramana", 1, 1);
+        // Station karajElSet = new Station("karaj el set", 1, 1);
 
-        HOME_STATION = new Station("rukn al dein", 1, 1);
+        // HOME_STATION = new Station("rukn al dein", 1, 1);
 
-        university.AddConnection(ConnectionType.BUS, jaramana, 1, 2, "unimana");
-        jaramana.AddConnection(ConnectionType.BUS, karajElSet, 1, 3, "jarElSet");
-        karajElSet.AddConnection(ConnectionType.BUS, HOME_STATION, 1, 2.5f, "karajElBeit");
+        // university.AddConnection(ConnectionType.BUS, jaramana, 1, 2, "unimana");
+        // jaramana.AddConnection(ConnectionType.BUS, karajElSet, 1, 3, "jarElSet");
+        // karajElSet.AddConnection(ConnectionType.BUS, HOME_STATION, 1, 2.5f, "karajElBeit");
 
-        State initialState = new State(station: university, timeSpent: 0, hp: 100, money: 1500);
+        // State initialState = new State(station: university, timeSpent: 0, hp: 100, money: 1500);
 
-        IComparer<StateInfo> comparer = new BestTimeGoal();
-        Solver.Solve(initialState, comparer);
+        // IComparer<StateInfo> comparer = new BestTimeGoal();
+        // Solver.Solve(initialState, comparer);
     }
 }
