ffc9affdd581e33a5e09558f4033d4e0ae0ebc7c
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-28 22:44:50 +0300

    Remove duplicate on foot connections from stations

--- Station/Station.cs
+++ Station/Station.cs
@@ -7,6 +7,8 @@ class Station
     public readonly StationLocation Location;
     public readonly List<Connection> Connections;
 
+    private HashSet<Connection> _onFootConnections;
+
     public Station(string name, float taxiWaitTime, float busWaitTime, StationLocation location)
     {
         Name = name;
@@ -15,14 +17,25 @@ class Station
         Location = location;
 
         Connections = new List<Connection>();
+        _onFootConnections = new HashSet<Connection>();
     }
 
     public void AddConnection(ConnectionType type, Station targetStation, float distanceInKm, float speedInKph, string routeName = "")
     {
         Connection connection = ConnectionFactory.GetConnection(type, targetStation, distanceInKm, speedInKph, routeName);
         Connections.Add(connection);
-        connection = ConnectionFactory.GetConnection(ConnectionType.ON_FOOT, targetStation, distanceInKm, 5.5f);
-        Connections.Add(connection);
+
+
+        // We're adding onFoot connections here because adding them manually from the main program
+        // is a pain in the back. To ensure that we only have one ON_FOOT_CONNECTION for each targetStation,
+        // we have the _onFootConnections HashSet.
+
+        Connection onFootConnection = ConnectionFactory.GetConnection(ConnectionType.ON_FOOT, targetStation, distanceInKm, speedInKph);
+        if (!_onFootConnections.Contains(onFootConnection))
+        {
+            Connections.Add(onFootConnection);
+            _onFootConnections.Add(onFootConnection);
+        }
     }
 
     public float GetWaitingTime(Connection connection)
@@ -46,6 +59,8 @@ class Station
         float distanceYSquared = MathF.Pow(Location.x - s.Location.x, 2);
         float distanceXSquared = MathF.Pow(Location.y - s.Location.y, 2);
 
-        return MathF.Sqrt(distanceXSquared + distanceYSquared) * 0.1f; //0.1 is to convert from coordinates to KM
+        float COORDINATE_TO_KM_CONVERSION_FACTOR = 0.1f;
+
+        return MathF.Sqrt(distanceXSquared + distanceYSquared) * COORDINATE_TO_KM_CONVERSION_FACTOR; 
     }
 }
