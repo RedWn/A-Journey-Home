ed70a8f68e0b83d7cb61a47decac13d040ace435
RedWn <redwaniscool@gmail.com>
2022-12-29 11:51:28 +0200

    Add new heuristic values
    As it turns out choosing the worse case for heuristics always yields better results

--- Solver/Heuristics.cs
+++ Solver/Heuristics.cs
@@ -6,8 +6,8 @@
     {
         float distance = a.CalcGeoDistTo(b);
 
-        float AVERAGE_SPEED_OF_TRANSPORTATION = 22.0f;
-        float time = distance / AVERAGE_SPEED_OF_TRANSPORTATION;
+        float MINIMUM_SPEED_OF_TRANSPORTATION = 5.5f;
+        float time = distance / MINIMUM_SPEED_OF_TRANSPORTATION;
 
         return time;
     };
@@ -16,8 +16,8 @@
     {
         float distance = a.CalcGeoDistTo(b);
 
-        float AVERAGE_HP_COST = -3.4f; // average of -10 for walking, -5 for buses, and +5 for taxis
-        float dHP = distance * AVERAGE_HP_COST;
+        float HP_COST = -10f;
+        float dHP = distance * HP_COST;
 
         return dHP;
     };
@@ -34,11 +34,11 @@
     public static readonly HeuristicCalculator allHeuristic = (a, b) =>
     {
         float distance = a.CalcGeoDistTo(b);
-        float AVERAGE_HP_COST = -3.4f;
-        float AVERAGE_SPEED_OF_TRANSPORTATION = 22.0f;
+        float HP_COST = -10f;
+        float MINIMUM_SPEED_OF_TRANSPORTATION = 5.5f;
 
         //0.33 is to give all three metrics similar weights (I don't know if this is legit or habd)
-        float all = distance * -1000 * 0.33f + distance * AVERAGE_HP_COST * 0.33f + distance / AVERAGE_SPEED_OF_TRANSPORTATION * 0.33f;
+        float all = distance * -1000 * 0.33f + distance * HP_COST * 0.33f + distance / MINIMUM_SPEED_OF_TRANSPORTATION * 0.33f;
 
         return all;
     };
