112e1ad9189394b08f96d80c9728904e366166a4
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-31 12:16:26 +0300

    Use PascalCase for station names

--- Program.cs
+++ Program.cs
@@ -1,54 +1,68 @@
 ﻿class Program
 {
     public static Station? HOME_STATION;
-
-
     public static void Main()
     {
-        Station university = new Station("University", 5 / 60, 20 / 60, new StationLocation(496, 321));
-        Station grandmother_garage = new Station("Grandmother Garage", 10 / 60, 5 / 60, new StationLocation(501, 308));
-        Station the_hardworker = new Station("The Hardworker", 2 / 60, 30 / 60, new StationLocation(500, 295));
-        Station coalers = new Station("Coalers", 2 / 60, 2 / 60, new StationLocation(508, 294)); ;
-        Station tartar_blasphemy = new Station("Tartar Blasphemy", 2, 10 / 60, new StationLocation(494, 273));
-        Station president_bridge = new Station("The President Bridge", 10 / 60, 15 / 60, new StationLocation(512, 288));
-        Station the_square = new Station("Ummayyad Square", 10 / 60, 20 / 60, new StationLocation(513, 276));
-        Station immigrants = new Station("The Immigrants", 10 / 60, 20 / 60, new StationLocation(521, 273));
-        Station corn_square = new Station("Corn Square", 15 / 60, 25 / 60, new StationLocation(521, 290));
-        Station the_garden = new Station("The Garden", 15 / 60, 25 / 60, new StationLocation(521, 282));
-        Station religon_corner = new Station("Religion's Corner", 20 / 60, 30 / 60, new StationLocation(531, 296));
-        Station consolation = new Station("Consolation", 10 / 60, 30 / 60, new StationLocation(511, 262));
-        Station beautiful_woman = new Station("Beautiful Woman", 2 / 60, 20 / 60, new StationLocation(500, 252));
-        Station project_destroyed = new Station("Project Destroyed", 2 / 60, 15 / 60, new StationLocation(531, 240));
+        Station University = new Station("University", 5 / 60, 20 / 60, new StationLocation(496, 321));
+        Station GrandmotherGarage = new Station("Grandmother Garage", 10 / 60, 5 / 60, new StationLocation(501, 308));
+        Station TheHardworker = new Station("The Hardworker", 2 / 60, 30 / 60, new StationLocation(500, 295));
+        Station Coalers = new Station("Coalers", 2 / 60, 2 / 60, new StationLocation(508, 294)); ;
 
-        university.AddConnection(ConnectionType.BUS, grandmother_garage, university.CalcGeoDistTo(grandmother_garage), 20, "industry");
-        grandmother_garage.AddConnection(ConnectionType.TAXI, coalers, grandmother_garage.CalcGeoDistTo(coalers), 40);
-        grandmother_garage.AddConnection(ConnectionType.BUS, the_hardworker, grandmother_garage.CalcGeoDistTo(the_hardworker), 20, "industry");
-        the_hardworker.AddConnection(ConnectionType.BUS, grandmother_garage, the_hardworker.CalcGeoDistTo(coalers), 20, "industry");
-        the_hardworker.AddConnection(ConnectionType.TAXI, tartar_blasphemy, the_hardworker.CalcGeoDistTo(tartar_blasphemy), 40);
-        the_hardworker.AddConnection(ConnectionType.TAXI, the_square, the_hardworker.CalcGeoDistTo(the_square), 40);
-        the_hardworker.AddConnection(ConnectionType.TAXI, beautiful_woman, the_hardworker.CalcGeoDistTo(beautiful_woman), 40);
-        coalers.AddConnection(ConnectionType.TAXI, tartar_blasphemy, coalers.CalcGeoDistTo(tartar_blasphemy), 40);
-        coalers.AddConnection(ConnectionType.BUS, president_bridge, coalers.CalcGeoDistTo(president_bridge), 20, "industry");
-        president_bridge.AddConnection(ConnectionType.BUS, the_square, president_bridge.CalcGeoDistTo(the_square), 20, "shitty");
-        president_bridge.AddConnection(ConnectionType.BUS, the_garden, president_bridge.CalcGeoDistTo(the_garden), 20, "industry");
-        president_bridge.AddConnection(ConnectionType.TAXI, corn_square, president_bridge.CalcGeoDistTo(corn_square), 40);
-        tartar_blasphemy.AddConnection(ConnectionType.TAXI, the_square, tartar_blasphemy.CalcGeoDistTo(the_square), 40);
-        the_square.AddConnection(ConnectionType.BUS, immigrants, the_square.CalcGeoDistTo(immigrants), 20, "circler");
-        the_square.AddConnection(ConnectionType.BUS, consolation, the_square.CalcGeoDistTo(consolation), 20, "shitty");
-        the_garden.AddConnection(ConnectionType.TAXI, corn_square, the_garden.CalcGeoDistTo(corn_square), 40);
-        the_garden.AddConnection(ConnectionType.BUS, immigrants, the_garden.CalcGeoDistTo(immigrants), 20, "industry");
-        corn_square.AddConnection(ConnectionType.TAXI, religon_corner, the_garden.CalcGeoDistTo(religon_corner), 40);
-        immigrants.AddConnection(ConnectionType.BUS, religon_corner, the_garden.CalcGeoDistTo(religon_corner), 20, "circler");
-        immigrants.AddConnection(ConnectionType.TAXI, project_destroyed, the_garden.CalcGeoDistTo(project_destroyed), 40);
-        consolation.AddConnection(ConnectionType.TAXI, beautiful_woman, the_garden.CalcGeoDistTo(beautiful_woman), 40);
-        consolation.AddConnection(ConnectionType.BUS, project_destroyed, the_garden.CalcGeoDistTo(project_destroyed), 20, "shitty");
-        beautiful_woman.AddConnection(ConnectionType.BUS, consolation, the_garden.CalcGeoDistTo(consolation), 20, "circler");
+        Station TartarBlasphemy = new Station("Tartar Blasphemy", 2, 10 / 60, new StationLocation(494, 273));
+        Station PresidentBridge = new Station("The President Bridge", 10 / 60, 15 / 60, new StationLocation(512, 288));
+        Station UmayyadSquare = new Station("Ummayyad Square", 10 / 60, 20 / 60, new StationLocation(513, 276));
 
-        State initialState = new State(station: university, timeSpent: 0, hp: 100, money: 5000);
-        HOME_STATION = immigrants;
+        Station Immigrants = new Station("The Immigrants", 10 / 60, 20 / 60, new StationLocation(521, 273));
+        Station CornSquare = new Station("Corn Square", 15 / 60, 25 / 60, new StationLocation(521, 290));
+        Station TheGarden = new Station("The Garden", 15 / 60, 25 / 60, new StationLocation(521, 282));
+        Station ReligionCorner = new Station("Religion's Corner", 20 / 60, 30 / 60, new StationLocation(531, 296));
+
+        Station Consolation = new Station("Consolation", 10 / 60, 30 / 60, new StationLocation(511, 262));
+        Station BeautifulWoman = new Station("Beautiful Woman", 2 / 60, 20 / 60, new StationLocation(500, 252));
+
+        Station ProjectDestroyed = new Station("Project Destroyed", 2 / 60, 15 / 60, new StationLocation(531, 240));
+
+        University.AddConnection(ConnectionType.BUS, GrandmotherGarage, University.CalcGeoDistTo(GrandmotherGarage), 20, "industry");
+
+        GrandmotherGarage.AddConnection(ConnectionType.TAXI, Coalers, GrandmotherGarage.CalcGeoDistTo(Coalers), 40);
+        GrandmotherGarage.AddConnection(ConnectionType.BUS, TheHardworker, GrandmotherGarage.CalcGeoDistTo(TheHardworker), 20, "industry");
+
+        TheHardworker.AddConnection(ConnectionType.BUS, GrandmotherGarage, TheHardworker.CalcGeoDistTo(Coalers), 20, "industry");
+        TheHardworker.AddConnection(ConnectionType.TAXI, TartarBlasphemy, TheHardworker.CalcGeoDistTo(TartarBlasphemy), 40);
+        TheHardworker.AddConnection(ConnectionType.TAXI, UmayyadSquare, TheHardworker.CalcGeoDistTo(UmayyadSquare), 40);
+        TheHardworker.AddConnection(ConnectionType.TAXI, BeautifulWoman, TheHardworker.CalcGeoDistTo(BeautifulWoman), 40);
+
+        Coalers.AddConnection(ConnectionType.TAXI, TartarBlasphemy, Coalers.CalcGeoDistTo(TartarBlasphemy), 40);
+        Coalers.AddConnection(ConnectionType.BUS, PresidentBridge, Coalers.CalcGeoDistTo(PresidentBridge), 20, "industry");
+
+        PresidentBridge.AddConnection(ConnectionType.BUS, UmayyadSquare, PresidentBridge.CalcGeoDistTo(UmayyadSquare), 20, "shitty");
+        PresidentBridge.AddConnection(ConnectionType.BUS, TheGarden, PresidentBridge.CalcGeoDistTo(TheGarden), 20, "industry");
+        PresidentBridge.AddConnection(ConnectionType.TAXI, CornSquare, PresidentBridge.CalcGeoDistTo(CornSquare), 40);
+
+        TartarBlasphemy.AddConnection(ConnectionType.TAXI, UmayyadSquare, TartarBlasphemy.CalcGeoDistTo(UmayyadSquare), 40);
+
+        UmayyadSquare.AddConnection(ConnectionType.BUS, Immigrants, UmayyadSquare.CalcGeoDistTo(Immigrants), 20, "circler");
+        UmayyadSquare.AddConnection(ConnectionType.BUS, Consolation, UmayyadSquare.CalcGeoDistTo(Consolation), 20, "shitty");
+
+        TheGarden.AddConnection(ConnectionType.TAXI, CornSquare, TheGarden.CalcGeoDistTo(CornSquare), 40);
+        TheGarden.AddConnection(ConnectionType.BUS, Immigrants, TheGarden.CalcGeoDistTo(Immigrants), 20, "industry");
+
+        CornSquare.AddConnection(ConnectionType.TAXI, ReligionCorner, TheGarden.CalcGeoDistTo(ReligionCorner), 40);
+
+        Immigrants.AddConnection(ConnectionType.BUS, ReligionCorner, TheGarden.CalcGeoDistTo(ReligionCorner), 20, "circler");
+        Immigrants.AddConnection(ConnectionType.TAXI, ProjectDestroyed, TheGarden.CalcGeoDistTo(ProjectDestroyed), 40);
+
+        Consolation.AddConnection(ConnectionType.TAXI, BeautifulWoman, TheGarden.CalcGeoDistTo(BeautifulWoman), 40);
+        Consolation.AddConnection(ConnectionType.BUS, ProjectDestroyed, TheGarden.CalcGeoDistTo(ProjectDestroyed), 20, "shitty");
+
+        BeautifulWoman.AddConnection(ConnectionType.BUS, Consolation, TheGarden.CalcGeoDistTo(Consolation), 20, "circler");
+
+        State initialState = new State(station: University, timeSpent: 0, hp: 50, money: 5000);
+        HOME_STATION = ProjectDestroyed;
 
         Heuristics.HeuristicCalculator c = Heuristics.timeHeuristic;
         IComparer<StatePriority> comparer = new BestTimeGoal();
+
         Solver.Solve(initialState, comparer, c);
     }
 }
